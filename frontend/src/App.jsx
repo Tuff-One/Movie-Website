7ac4ffb83ea5a400906c7b62b9a663a0bd3dea85
Atuhe Kambonde <atuhekambonde@gmail.com>
2026-07-16 11:09:57 +0200

    Home page setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
-import MovieCard from "./components/MovieCard"
+import Home from "./pages/Home"
 
 function App() {
+  const movieNumber = 2;
+
   return (
     <>
-     <MovieCard movie={{title: "Atu's Film", release_date: "2025"}} />
-      <MovieCard movie={{title: "Joe's Film", release_date: "2025"}} />
+    <Home />
     </>
   )
 }
