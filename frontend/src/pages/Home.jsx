7d190f3fe866ec5da0b1b244d094e6446c45f84a
Atuhe Kambonde <atuhekambonde@gmail.com>
2026-07-16 12:26:07 +0200

    search basr with rendering State function

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,40 @@
 import MovieCard from "../components/MovieCard";
+import {useState} from "react";
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   const movies = [
     {id: 1, title: "John Wick", release_date: "2020"},
     {id: 2, title: "The Matrix", release_date: "1999"},
     {id: 3, title: "The Game Plan", release_date: "2003"},
     {id: 4, title: "The Rip", release_date: "2026"}
-  ]
+  ];
 
-  return <div className = "home">
+  const handleSearch = (e) => {
+      e.preventDefault();
+      alert(searchQuery);
+  };
+
+  return (
+  <div className = "home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input type="text" 
+        placeholder="Search for movies..." 
+        className="search-input" 
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}/>
+        <button type="submit" className="search-button">Search</button>
+      </form>
+
     <div className="movies-grid">
-      {movies.map((movie) => (
+      {movies.map((movie) => 
+        movie.title.toLowerCase().startsWith(searchQuery) && (
         <MovieCard movie={movie} key={movie.id} />
       ))}
     </div>
   </div>
+  );
 }
 
 export default Home;
